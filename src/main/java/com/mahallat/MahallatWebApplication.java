622a2bab6a3ebfa4c3a9f02efe783e1119340686
abed bilani <abedelaziz.bilani@isae.edu.lb>
2018-08-16 17:37:21 +0300

    convert to war

--- src/main/java/com/mahallat/MahallatWebApplication.java
+++ src/main/java/com/mahallat/MahallatWebApplication.java
@@ -2,11 +2,18 @@ package com.mahallat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MahallatWebApplication {
+public class MahallatWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MahallatWebApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MahallatWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MahallatWebApplication.class, args);
+    }
 }
